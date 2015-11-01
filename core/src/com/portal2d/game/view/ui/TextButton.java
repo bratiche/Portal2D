95c80deb0eb442b28c359768a5f6b5d7b3584be2
matias <maperazzo@itba.edu.ar>
2015-11-01 06:04:55 -0300

    MenuView is Stronger now

--- core/src/com/portal2d/game/view/ui/TextButton.java
+++ core/src/com/portal2d/game/view/ui/TextButton.java
@@ -1,8 +1,14 @@
 package com.portal2d.game.view.ui;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
+import com.badlogic.gdx.math.Vector3;
+
+import static com.portal2d.game.view.ViewConstants.VIEWPORT_WIDTH;
+
 
 /**
  * TODO
@@ -11,18 +17,38 @@ public class TextButton extends UIComponent {
 
     private String text;
     private BitmapFont font;
+    private GlyphLayout gl;
 
-    @Override
-    public void render(SpriteBatch batch) {
+    // Normal button in screen
+    public TextButton ( float x, float y, float width, float height, String text, BitmapFont font) {
+        super(x, y, width, height);
+        this.font=font;
+        this.text=text;
+        gl = new GlyphLayout(font, text);
+    }
+
+    // Centered button in screen on y position
+    public TextButton ( float y, float width, float height, String text, BitmapFont font) {
+        this(0, y, width, height, text, font);
+        this.x= VIEWPORT_WIDTH/2 - gl.width/2;
 
     }
 
-    public boolean isClicked() {
-        if(Gdx.input.isTouched()) {
-            float x = Gdx.input.getX();
-            float y = Gdx.input.getY();
+    @Override
+    public void render(SpriteBatch batch) {
+        batch.begin();
 
-            return x >= this.x && y >= this.y && x <= this.x + width && y <= this.y + height;
+        font.setColor(1.0f, 1.0f, 1.0f, 1.0f);
+        font.draw(batch, text, x, y);
+        batch.end();
+    }
+
+    public boolean isClicked(float x, float y) {
+        if(Gdx.input.isTouched()) {
+
+            System.out.println( x + " , " + this.x+ " AND "+ y + " , " +this.y );
+            System.out.println(x >= this.x - width && y >= this.y - width && x <= this.x + gl.width + width  && y <= this.y + gl.height + height);
+            return x >= this.x - width && y >= this.y - gl.height - width && x <= this.x + gl.width + width  && y <= this.y + height;
         }
         return false;
     }
